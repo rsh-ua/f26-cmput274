09de26738688a572de4a1fa490c581f5fb5681ab
Rob Hackman <rhackman@ualberta.ca>
2026-09-15 22:35:55 -0600

    Pushing a1

---
+++ a1/provided/q4.py
@@ -0,0 +1,22 @@
+from cmput274 import *
+# The above line should appear at the start of
+# all of your CMPUT274 programs
+
+def digitReplace(n, target, replace):
+  # Remove the "pass" line and fill in this function.
+  # Don't forget to include your function specification
+  # docstring at the beginning of the function!
+  pass
+
+
+def main():
+  testExact("basic1", 179095, digitReplace, 172025, 2, 9)
+  testExact("basic2", 772777, digitReplace, 112111, 1, 7)
+  testExact("basic3", 98402521, digitReplace, 98402521, 3, 9)
+  # Write your own test cases here as you see fit.
+  # You should write many test cases to make yourself
+  # confident your solution works before you hand it in!
+  runTests()
+
+if __name__ == "__main__":
+  main()
